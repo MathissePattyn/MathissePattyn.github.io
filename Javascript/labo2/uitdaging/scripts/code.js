1adc346f86c8c9e4f15ac2f817c4848dfbb734d8
MathissePattyn <mathisse.pattyn@student.vives.be>
2025-02-18 12:09:20 +0100

    1

--- Javascript/labo2/uitdaging/scripts/code.js
+++ Javascript/labo2/uitdaging/scripts/code.js
@@ -1,9 +1,9 @@
 const setup = () => {
     let btnZonder = document.getElementById('btnZonder');
-    btnZonder.addEventListener('click', zonderBulletpoints);
+    btnZonder.addEventListener('click', zonder);
 
     let btnMet = document.getElementById('btnMet');
-    btnMet.addEventListener('click', metBulletpoints);
+    btnMet.addEventListener('click', met);
 
 }
 
@@ -20,6 +20,22 @@ const metBulletpoints = () => {
     for (let item of zonderBullets) {
         item.className = "listItem";
     }
+}
+
+const met = () => {
+    let listItems = document.getElementsByClassName('listItem');
+    console.log(listItems[0].className);
+    listItems[0].classList.add('listItem');
+    listItems[0].classList.remove('zonderBullets');
+    console.log(listItems[0].className);
+
+}
+
+const zonder = () => {
+    let listItems = document.getElementsByClassName('listItem');
+    console.log(listItems[0].className);
+    listItems[0].classList.add('zonderBullets');
+    console.log(listItems[0].className);
 }
 
 
