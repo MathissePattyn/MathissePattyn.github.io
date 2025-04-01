a36ead5b887950e00ee6d4b2f11850b9ce366863
MathissePattyn <mathisse.pattyn@student.vives.be>
2025-04-01 15:44:01 +0200

    1

--- Javascript/labo7/MatchingGame/MatchingGame2/scripts/code.js
+++ Javascript/labo7/MatchingGame/MatchingGame2/scripts/code.js
@@ -4,35 +4,51 @@ let global = {
     IMAGE_PATH_SUFFIX: ".png"
 };
 
+let kaartenArray = [];
 
 const setup = () => {
+
     let btnStart = document.getElementById("btnStart");
     btnStart.addEventListener("click", startSpel);
 
 }
 
 let startSpel = () => {
-
+    console.log("Starting Spel");
+    let kaartSpel = document.getElementById("kaartSpel");
+    kaartSpel.innerHTML = "";
     maakKaartSpel();
 }
 
-let maakKaartSpel = () => {
+let maakKaartenArray = () => {
+    kaartenArray = [];
     let arrayKaarten = [];
     for(let i = 1; i<global.AANTAL_KAARTEN+1;i++){
         arrayKaarten.push(global.IMAGE_PATH_PREFIX + i + global.IMAGE_PATH_SUFFIX);
     }
-    console.log(arrayKaarten);
-
-    let kaartenArray = [...arrayKaarten, ...arrayKaarten];
-
+    kaartenArray = [...arrayKaarten, ...arrayKaarten];
     console.log(kaartenArray);
+}
+
+let maakKaartSpel = () => {
+    maakKaartenArray();
 
     let kaartSpel = document.getElementById("kaartSpel");
     for(let i = 0; i < kaartenArray.length; i++){
         let kaartContainer = document.createElement("div");
-        let kaart = document.createElement("img");
-        kaart.setAttribute("src", kaartenArray[i]);
-        kaartContainer.appendChild(kaart);
+        kaartContainer.setAttribute("class", "kaart");
+
+        let voorkantKaart = document.createElement("img");
+        voorkantKaart.setAttribute("class", "voorkant");
+        voorkantKaart.setAttribute("src", kaartenArray[i]);
+        kaartContainer.appendChild(voorkantKaart);
+
+        let achterkantKaart = document.createElement("img");
+        achterkantKaart.setAttribute("class", "achterkant");
+        achterkantKaart.setAttribute("src", "images/achterkant.png");
+        kaartContainer.appendChild(achterkantKaart);
+
+
         kaartSpel.appendChild(kaartContainer);
     }
 
